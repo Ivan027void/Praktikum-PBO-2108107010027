05c6023c1e7fb480d7dca4a793e6c52d727fa8a5
Ivan Chiari <ahinivan@gmail.com>
2023-02-06 19:30:23 +0700

    feat new variabel and plus operation

--- a1.c
+++ a1.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 
 int main(){
+ int a= 5;
+ int b=10;
+
+
     printf("hello world");
-}
+    int jumlah =a +b;
+    printf("\n jumlah dari a+b adalah %d", jumlah);
+    
+}   
